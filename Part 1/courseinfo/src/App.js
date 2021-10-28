ca5db08437076abd9874dcbf2bd2eedafadc2f08
Helge Kim <met.oleg@protonmail.ch>
2021-10-28 18:20:33 +0500

    Part 1: 1.4

--- Part 1/courseinfo/src/App.js
+++ Part 1/courseinfo/src/App.js
@@ -2,24 +2,27 @@ import React from 'react'
 function App() {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
   return (
     <div>
      <Header course={course}/>
-     <Content list={[part1, part2, part3]}/>
-     <Total list={[part1, part2, part3]}/>
+     <Content list={parts}/>
+     <Total list={parts}/>
     </div>
   )
 }
