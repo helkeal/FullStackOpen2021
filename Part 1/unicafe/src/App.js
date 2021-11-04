e9af9004351e2362924a5b85bc0fe60683b87fe4
Helge Kim <met.oleg@protonmail.ch>
2021-11-04 13:59:53 +0500

    1.11

--- Part 1/unicafe/src/App.js
+++ Part 1/unicafe/src/App.js
@@ -12,21 +12,45 @@ function App() {
 
   return(
     <div>
+      <Buttons controller = {setPoints} points = {points}/>
       <Statistics points = {points}/>
     </div>
+  )
+}
+
+function Buttons({controller, points}) {
+
+  let properties = Object.keys(points);
+
+  return(
+    <div>
+      <h1>give feedback</h1>
+      {properties.map(element => <Button setter={controller}  key = {Math.random() * 10}points = {points} text = {element}/>)}
+    </div>
+  )
+}
+
+function Button({text, points, setter}) {
+
+  function handleClick() {
+      let object = {...points};
+      object[text] = points[text] + 1;
+      setter(object);
+  }
+
+  return (
+      <button onClick = {handleClick}>{text}</button>
   )
 }
 
 function Statistics({points}) {
 
   let total = Object.values(points).reduce((a, b) => a + b);
-  console.log(total);
-
-  if (total == 0)  {
+  if (total === 0)  {
     return(
     <div>
       <h2>statistics</h2>
-      <p>No elements to show</p>
+      <p>No feedback given</p>
     </div>
   )} 
   
@@ -35,7 +59,7 @@ function Statistics({points}) {
     return(
       <div>
         <h2>statistics</h2>
-        {Object.keys(points).map(element => <StatisticsLine text={element} point={points[element]}/>)}
+        {Object.keys(points).map(element => <StatisticsLine key={Math.random() * 10}text={element} point={points[element]}/>)}
       </div>
     )
 
