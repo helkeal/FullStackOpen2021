e19a26a8d395b0ca99c00f2d11352a30b8030da5
Helge Kim <met.oleg@protonmail.ch>
2021-11-05 20:56:36 +0500

    Part 2: 2.5

--- Part 2/courseinfo/src/App.js
+++ Part 2/courseinfo/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Course from './Course';
 function App() {
 
   const courses = [
@@ -50,48 +51,6 @@ function App() {
 
 }
 
-function Course({course}) {
-  return (
-    <div>
-     <Header course={course['name']}/>
-     <Content parts={course.parts}/>
-     <Total parts={course.parts}/>
-    </div>
-  )
-}
-
-
-function Header(props) {
-    return(
-     <div>
-     <h1> {props.course} </h1>
-     </div>
-    )
-}
-
-function Content(props) {
-    return (
-     <div>
-        {props.parts.map(element => <Part name={element.name} exercises={element.exercises}/>)}
-     </div>
-    )
-}
-
-function Part(props) {
-    return(
-        <div>
-        <p>{props.name}:{props.exercises} </p>
-        </div>
-    )
-}
-
-function Total(props) {
-    return (
-     <div>
-        <p>Number of exercises: {props.parts.map(element => element.exercises).reduce((a, b) => a + b)}</p>
-     </div>
-    )
-}
 
 
 
---
+++ Part 2/courseinfo/src/Course.js
@@ -0,0 +1,45 @@
+import React from 'react';
+function Course({course}) {
+    return (
+      <div>
+       <Header course={course['name']}/>
+       <Content parts={course.parts}/>
+       <Total parts={course.parts}/>
+      </div>
+    )
+  }
+  
+  
+  function Header(props) {
+      return(
+       <div>
+       <h1> {props.course} </h1>
+       </div>
+      )
+  }
+  
+  function Content(props) {
+      return (
+       <div>
+          {props.parts.map(element => <Part name={element.name} exercises={element.exercises}/>)}
+       </div>
+      )
+  }
+  
+  function Part(props) {
+      return(
+          <div>
+          <p>{props.name}:{props.exercises} </p>
+          </div>
+      )
+  }
+  
+  function Total(props) {
+      return (
+       <div>
+          <p>Number of exercises: {props.parts.map(element => element.exercises).reduce((a, b) => a + b)}</p>
+       </div>
+      )
+  }
+  
+export default Course;  
