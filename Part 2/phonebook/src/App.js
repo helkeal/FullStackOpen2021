2f00c38f664e47643f9e2645d456fde02b3f0f57
Helge Kim <met.oleg@protonmail.ch>
2021-11-06 09:55:50 +0500

    2.10

---
+++ Part 2/phonebook/src/App.js
@@ -0,0 +1,112 @@
+import {useState} from 'react';
+
+function App() {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [shownPersons, setShownPersons] = useState(persons)
+
+  return (
+    <div>
+      <Filter persons={shownPersons} oldPersons={persons} setPersons={setShownPersons} />
+      <PersonForm persons={persons} setPersons={setPersons} />
+      <Display persons={persons} shown={shownPersons}/>
+    </div>
+  );
+}
+
+
+function Filter({persons, oldPersons, setPersons}) {
+
+  const [value, handleValue] = useState("")
+
+  function find(event) {
+    handleValue(event.target.value);
+    if (value.length >= 3) {
+      setPersons(persons.filter(person => person.name.match(value) || person.number.match(value)))
+    } else {
+      setPersons(oldPersons);
+    }
+  }
+
+  return(
+    <div>
+      <h1>Filter</h1>
+      <input value={value} onChange={find}/>
+    </div>
+  )
+}
+
+function PersonForm({persons, setPersons}) {
+
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("")
+
+  function create(event) {
+    event.preventDefault()
+
+    if (persons.find(element => element.name.match(name))) {
+      alert("This contact is already in the phonebook")
+    } else {
+      let contact = {
+        name: name,
+        number: number,
+        id: persons.length
+      }
+      setPersons([...persons, contact]);
+      console.log(persons)
+    }
+  }
+
+  return(
+    <div>
+      <h1>Add your contact</h1>
+      <div>
+        <form onSubmit={create}> 
+        <fieldset>
+         <label>
+           <p>Name</p>
+           <input value={name} onChange={(event) => setName(event.target.value)} />
+         </label>
+         <label>
+           <p>Number</p>
+           <input value={number} onChange={(event) => setNumber(event.target.value)} />
+         </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+function Display({persons, shown}) {
+  if (persons.length === shown.length)
+  {return(
+    <div>
+      <h1>Persons</h1>
+      {persons.map(element => <Person person={element}/>)}
+    </div>
+  )} else {
+    return(
+    <div>
+      <h1>Persons</h1>
+      {shown.map(element => <Person person={element}/>)}
+    </div>
+    )
+  }
+}
+
+function Person({person}) {
+  return(
+    <p>{person.name}: {person.number}</p>
+  )
+}
+
+
+export default App;
