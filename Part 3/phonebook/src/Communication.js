0dc26d0c79bda9a94dc9bdec538fc7a0c0783ef1
Helge Kim <met.oleg@protonmail.ch>
2021-11-18 16:12:10 +0500

    Part 3: 3.14; not functioning yet

--- Part 3/phonebook/src/Communication.js
+++ Part 3/phonebook/src/Communication.js
@@ -1,27 +1,47 @@
-import axios from 'axios';
+const database = require('mongoose')
 
-let baseURL = "/persons"
+let baseURL = process.env.DB_URL;
+
+const schema = new database.Schema(
+ {
+  name: String,
+  number: String,
+  date: Date
+ }
+);
+
+const Contact = new database.model('Contact', schema)
+
+
+database.connect(baseURL).catch(error =>
+  {
+   console.log("Connection impossible")
+   console.log(error)
+   process.exit();
+  })
 
 
 function Add(object) {
- return axios.post(baseURL, object)
+ return "naught"
 }
 
 function Get() {
- return axios.get(baseURL)
+ return  Contact.find({})
 }
-
+/*
 function Delete(object) {
  return axios.delete(`${baseURL}/${object.id}`)
 }
 
+ delete: Delete,
+ update: Update
+ 
 function Update(id, object) {
  return axios.put(`${baseURL}/${id}`, object)
 }
+*/
 
 export default  {
  add: Add,
  get: Get,
- delete: Delete,
- update: Update
 }
