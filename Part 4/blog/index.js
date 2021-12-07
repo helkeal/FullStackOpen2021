9de17b574f595c7ba2bfc0fbe42b7ead5d148dd1
Helge Kim <met.oleg@protonmail.ch>
2021-12-07 18:54:52 +0500

    Modules extracted

--- Part 4/blog/index.js
+++ Part 4/blog/index.js
@@ -1,41 +1,13 @@
 const http = require('http')
 const express = require('express')
-const app = express()
+const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose')
 const configuration = require('./utils/config.js')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
 const mongoUrl = configuration.URL;
-mongoose.connect(mongoUrl)
+mongoose.connect(mongoUrl);
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = configuration.PORT;
 app.listen(PORT, () => {
